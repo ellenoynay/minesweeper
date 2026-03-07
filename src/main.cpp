ceb95b203a0d3350c93b7b6e86b717391778184e
Elle (Steffi) Noynay <ellenoynay@gmail.com>
2026-03-07 02:08:23 -0800

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,3 @@ int main() {
 
     return 0;
 }
-
-// clang++ -std=c++17 Square.cpp Cell.cpp main.cpp -o builds/sfml-app \
-//  -I/opt/homebrew/opt/sfml/include \
-//  -L/opt/homebrew/opt/sfml/lib \
-//  -lsfml-graphics -lsfml-window -lsfml-system
